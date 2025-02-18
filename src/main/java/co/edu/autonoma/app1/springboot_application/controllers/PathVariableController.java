182691d3812b807bdd5c83eb68baf8da6de6a883
Germán Arenas <german.arenass@autonoma.edu.co>
2025-02-17 21:52:49 -0500

    solicitud POST

--- src/main/java/co/edu/autonoma/app1/springboot_application/controllers/PathVariableController.java
+++ src/main/java/co/edu/autonoma/app1/springboot_application/controllers/PathVariableController.java
@@ -2,8 +2,14 @@ package co.edu.autonoma.app1.springboot_application.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.autonoma.app1.springboot_application.models.Empleados;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/variable")
@@ -14,6 +20,12 @@ public class PathVariableController {
         ParametroDTO parametro1 = new ParametroDTO();
         parametro1.setInformacion(mensaje);
         return parametro1;
+    }
+
+    @PostMapping("/solicitud")
+    public Empleados creaEmpleado(@RequestBody Empleados empleado1){
+
+        return empleado1;
     }
 
 }
