5db427ca5b9d1927b95fde83e1cc55f67a2c2493
Germán Arenas <german.arenass@autonoma.edu.co>
2025-02-17 21:24:18 -0500

    path variable

---
+++ src/main/java/co/edu/autonoma/app1/springboot_application/controllers/PathVariableController.java
@@ -0,0 +1,19 @@
+package co.edu.autonoma.app1.springboot_application.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api/variable")
+public class PathVariableController {
+
+    @GetMapping("/pagina1/{mensaje}")
+    public ParametroDTO pagina1(@PathVariable String mensaje) {
+        ParametroDTO parametro1 = new ParametroDTO();
+        parametro1.setInformacion(mensaje);
+        return parametro1;
+    }
+
+}
